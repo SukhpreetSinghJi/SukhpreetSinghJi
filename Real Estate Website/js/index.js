3e1e88a875a8947b907e2582a5094775237400cb
SukhpreetSinghJi <72065114+SukhpreetSinghJi@users.noreply.github.com>
2021-08-28 22:05:02 +0530

    Delete Real Estate Website directory

--- Real Estate Website/js/index.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(" .hamburger" );
-const navList = document.querySelector(" .nav-list ");
-
-if (hamburger) {
-    hamburger.addEventListener("click", () => {
-        navList.classList.toggle("open");
-    })
-}
